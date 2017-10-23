59855f4169c26e2c5f1bdc54e1f9d52aec1f312f
JBM <bumkuns@gmail.com>
2017-10-23 14:19:07 +0900

    firebase

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDpFrV-PesF1vICFcAVgldRk5Feurfs2Io",
+    apiKey: " ",//"AIzaSyDpFrV-PesF1vICFcAVgldRk5Feurfs2Io",
     authDomain: " ",
-    databaseURL: "https://fir-practice-36b52.firebaseio.com/",
-    projectID: "fir-practice-36b52",
+    databaseURL: " ",//https://fir-practice-36b52.firebaseio.com/",
+    projectID: " ",//"fir-practice-36b52",
     storageBucket: " ",
     messagingSensderID: " ",
 };
